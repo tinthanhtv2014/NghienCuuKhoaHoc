40011685f938e023302c6f8f7089f5627cb0eddf
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-06-28 23:10:54 +0700

    Thêm 1 vài ghi chú cho code

--- backend-nghiencuukhoahoc/src/routes/AdminRoute/adminRoute.js
+++ backend-nghiencuukhoahoc/src/routes/AdminRoute/adminRoute.js
@@ -7,6 +7,7 @@ const {
 } = require("../../controllers/AdminController/adminController.js");
 
 //login cho admin
+// Phần hàm này đang có tranh cãi với Phúc 28/6/2024
 const taiKhoanRote = (app) => {
 
   router.get("/tai-khoan", getAllTaiKhoanController);
